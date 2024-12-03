2f6659214f98463029fc8498bb5aea765b7efecd
WICKY-Arthur <arthur.wicky@edu.univ-fcomte.fr>
2024-12-03 15:32:53 +0100

    lightbox js et css TD
    lightbox js et css TD

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,16 @@
 // *** LIGHTBOX ***
+const lightbox = document.querySelector('#lightbox');
+const listThumbnail = document.querySelectorAll("[data-full-img]");
+const lightboxImg = document.querySelector('#lightbox > img');
+
+listThumbnail.forEach((thumbnail) => {
+    thumbnail.addEventListener('click', (evt) => {
+        lightboxImg.src = thumbnail.dataset.fullImg;
+        lightbox.showModal();
+    })
+});
+
+lightbox.addEventListener('click', () => {
+    lightbox.close();
+});
+
